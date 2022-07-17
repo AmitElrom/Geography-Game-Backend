29f7f2f18e11d51452e2bbee241c461618635adc
Amit Elrom <amitelrom99@gmail.com>
2022-07-17 16:51:19 +0300

    first push of backend to git

---
+++ routers/countriesRouter.js
@@ -0,0 +1,50 @@
+const axios = require('axios');
+const express = require('express');
+const router = express.Router();
+
+const { countries } = require('../countries.json');
+
+router.route('/')
+    .get((req, res) => {
+        try {
+            const { query: { minknown, maxknown } } = req;
+
+            const allCountries = countries;
+            if (minknown && maxknown) {
+                if (maxknown >= minknown) {
+                    const countries1 = allCountries.filter(country => {
+                        return country.flagKnown >= +minknown && country.flagKnown <= +maxknown
+                    })
+                    res.status(200).json(countries1)
+                    return
+                } else {
+                    res.status(400).json({
+                        error: `definitional error - maxKnown must be bigger or equal to minKnown`
+                    })
+                    return
+                }
+            }
+            else if (minknown && !maxknown) {
+                const countries1 = allCountries.filter(country => {
+                    return country.flagKnown >= +minknown
+                })
+                res.status(200).json(countries1)
+                return
+            }
+            else if (maxknown && !minknown) {
+                const countries1 = allCountries.filter(country => {
+                    return country.flagKnown <= +maxknown
+                })
+                res.status(200).json(countries1)
+                return
+            }
+            res.status(200).json(allCountries)
+            return
+        } catch (error) {
+            res.status(400).json({
+                error: error.message
+            })
+        }
+    })
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,27 @@
+// const axios = require('axios');
+
+// const cors = require('cors');
+// const express = require('express');
+// const app = express();
+
+// app.use(cors())
+// app.use(express.json())
+
+// app.get('/', async (req, res) => {
+//     const { data: countriesElrom } = await axios.default.get('http://localhost:8000/countries-elrom');
+//     const { data: countriesAPI } = await axios.default.get('https://restcountries.com/v2/all?fields=numericCode,flag');
+//     const newCountries = countriesElrom.map(country => {
+//         const countryAPI = countriesAPI.find(x => x.flag === country.flag);
+//         if (countryAPI !== null) {
+//             return {
+//                 ...country,
+//                 id: countryAPI.numericCode
+//             }
+//         }
+//     })
+//     res.json(newCountries)
+// })
+
+// app.listen(7000, () => {
+//     console.log('server is listening to port 7000');
+// })
