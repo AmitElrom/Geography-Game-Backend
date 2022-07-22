43d7c352c61b79315d805e89e3a1f010be63af1d
Amit Elrom <amitelrom99@gmail.com>
2022-07-22 16:17:29 +0300

    state building countries' flags' similarity data

--- server.js
+++ server.js
@@ -7,15 +7,29 @@
 // app.use(cors())
 // app.use(express.json())
 
+// // app.get('/', async (req, res) => {
+// //     const { data: countriesElrom } = await axios.default.get('http://localhost:8000/countries-elrom');
+// //     const { data: countriesAPI } = await axios.default.get('https://restcountries.com/v2/all?fields=numericCode,flag');
+// //     const newCountries = countriesElrom.map(country => {
+// //         const countryAPI = countriesAPI.find(x => x.flag === country.flag);
+// //         if (countryAPI !== null) {
+// //             return {
+// //                 ...country,
+// //                 id: countryAPI.numericCode
+// //             }
+// //         }
+// //     })
+// //     res.json(newCountries)
+// // })
+
 // app.get('/', async (req, res) => {
 //     const { data: countriesElrom } = await axios.default.get('http://localhost:8000/countries-elrom');
-//     const { data: countriesAPI } = await axios.default.get('https://restcountries.com/v2/all?fields=numericCode,flag');
 //     const newCountries = countriesElrom.map(country => {
-//         const countryAPI = countriesAPI.find(x => x.flag === country.flag);
-//         if (countryAPI !== null) {
-//             return {
-//                 ...country,
-//                 id: countryAPI.numericCode
+//         return {
+//             ...country,
+//             similarity: {
+//                 similarity1: [],
+//                 similarity2: []
 //             }
 //         }
 //     })
