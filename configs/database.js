ddde264eccf4224144471dcf2c9b51ec9b026512
Amit Elrom <amitelrom99@gmail.com>
2023-01-27 11:02:56 +0200

    change db connection resp

--- configs/database.js
+++ configs/database.js
@@ -1,4 +1,4 @@
 const { connect } = require('mongoose');
 
-connect(process.env.DB_URL).then(data => console.log(data)).catch(error => console.log(error));
+connect(process.env.DB_URL).then(data => console.log("server connected to database successfully")).catch(error => console.log(error));
 
